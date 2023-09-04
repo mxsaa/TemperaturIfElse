6949ae09ce249bea9f527b8e58e2198d93f84d16
yes <113014260+mxsaa@users.noreply.github.com>
2023-09-04 21:00:08 +0200

    Add files via upload

---
+++ VisualStudio/TemperaturIfElseWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturIfElseWhile
+double temp;
+
+while (true)
+{
+    Console.Write("Skriv inn temperatur: ");
+
+    if (double.TryParse(Console.ReadLine(), out temp))
+    {
+        if (temp > 0)
+        {
+            Console.WriteLine("Temperaturen er positiv.");
+        }
+        else if (temp < 0)
+{
+    Console.WriteLine("Temperaturen er negativ.");
+}
+else
+{
+    Console.WriteLine("Temperaturen er null.");
+}
+
+break;
+    }
+    else
+{
+    Console.WriteLine("Ugyldig verdi er inntastet. Prøv igjen.");
+}
+}
+
